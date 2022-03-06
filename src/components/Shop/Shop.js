48c1293b849521759d70535cd7c18d6ba4c2cc8b
dipdastoko <dipdastoko@gmail.com>
2022-03-06 15:58:08 +0600

    localStorage

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -8,13 +9,32 @@ const Shop = () => {
     const [cart, setCart] = useState([]);
 
     useEffect(() => {
+        console.log('product api called');
         fetch('./products.JSON')
             .then(res => res.json())
-            .then(data => setProducts(data));
-    }, [])
+            .then(data => {
+                setProducts(data);
+                console.log('products received');
+            });
+    }, []);
+    useEffect(() => {
+        console.log('Local Storage Called');
+        if (products.length) {
+            const savedCart = getStoredCart();
+            const storedCart = [];
+            for (const key in savedCart) {
+                // console.log(key);
+                const addedProduct = products.find(product => product.key === key);
+                storedCart.push(addedProduct);
+            }
+            setCart(storedCart);
+        }
+    }, [products]);
     const handleAddToCart = product => {
         const newCart = [...cart, product];
         setCart(newCart);
+
+        addToDb([product.key]);
     }
     return (
         <div className='shop-container'>
