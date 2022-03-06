1a3985b7f8691f0d05d6303b8d8c9dbae442ae5a
dipdastoko <dipdastoko@gmail.com>
2022-03-07 01:14:08 +0600

    changes

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -9,23 +9,26 @@ const Shop = () => {
     const [cart, setCart] = useState([]);
 
     useEffect(() => {
-        console.log('product api called');
         fetch('./products.JSON')
             .then(res => res.json())
             .then(data => {
                 setProducts(data);
-                console.log('products received');
             });
     }, []);
     useEffect(() => {
-        console.log('Local Storage Called');
         if (products.length) {
             const savedCart = getStoredCart();
             const storedCart = [];
             for (const key in savedCart) {
-                // console.log(key);
+                console.log(savedCart[key]);
                 const addedProduct = products.find(product => product.key === key);
-                storedCart.push(addedProduct);
+                if (addedProduct) {
+                    const quantity = savedCart[key];
+                    addedProduct.quantity = quantity;
+                    console.log(addedProduct);
+                    storedCart.push(addedProduct);
+                }
+
             }
             setCart(storedCart);
         }
