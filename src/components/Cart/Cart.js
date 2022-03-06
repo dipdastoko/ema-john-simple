0d7e5411e12cd1574e33bc329831cbaf856e7264
dipdastoko <dipdastoko@gmail.com>
2022-03-06 14:11:24 +0600

    cart updated

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,11 +7,18 @@ const Cart = (props) => {
     for (const product of cart) {
         total = total + product.price;
     }
+    const shipping = total > 0 ? 15 : 0;
+    const tax = (total + shipping) * 0.10;
+    const grandTotal = total + shipping + tax;
     return (
         <div>
             <h3>Order Summary</h3>
             <h5>Items Ordered: {props.cart.length}</h5>
-            <p>Total:{total}</p>
+            <p>Total:{total.toFixed(2)}</p>
+            <p>Shipping: {shipping}</p>
+            <p>tax: {tax.toFixed(2)}</p>
+            <br />
+            <p>Grand Total: {grandTotal.toFixed(2)}</p>
 
         </div>
     );
