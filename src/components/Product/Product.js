fe630311c32c1882f5efff75dd02492f23b9e83b
dipdastoko <dipdastoko@gmail.com>
2022-03-09 01:36:05 +0600

    star

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css';
+import Rating from 'react-rating';
 
 const Product = (props) => {
     // console.log(props);
-    const { name, price, img, stock, seller } = props.product;
+    const { name, price, img, stock, seller, star } = props.product;
 
     return (
         <div className='product'>
@@ -17,11 +18,18 @@ const Product = (props) => {
                 <p><small>by: {seller}</small></p>
                 <p>Price: {price}</p>
                 <p><small>only {stock} left in stock - order soon</small></p>
+                <Rating
+                    initialRating={star}
+                    emptySymbol="fa fa-star-o fa-2x icon-color"
+                    fullSymbol="fa fa-star fa-2x icon-color"
+                    readonly
+                />
+                <br />
                 <button onClick={() => {
                     props.handleAddToCart(props.product)
                 }} className='btn-regular'><FontAwesomeIcon icon={faShoppingCart} /> Add to cart</button>
             </div>
-        </div>
+        </div >
     );
 };
 
